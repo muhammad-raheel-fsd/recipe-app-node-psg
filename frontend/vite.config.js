19b3e6fadb96e7fd51db05fb6101b745410ca82c
zohaibmunawar <raheeluafbrw@gmail.com>
2024-07-17 19:53:44 +0500

    fix: port issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,14 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // env variables go here
-    // "process.env.VITE_API_ENDPOINT": JSON.stringify(
-    //   process.env.VITE_API_ENDPOINT
-    // ),
+  server: {
+    host: "0.0.0.0",
+    port: process.env.VITE_PORT || 5173,
   },
+  // define: {
+  // env variables go here
+  // "process.env.VITE_API_ENDPOINT": JSON.stringify(
+  //   process.env.VITE_API_ENDPOINT
+  // ),
+  // },
 });
