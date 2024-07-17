9e98acfc596f35f44990f200c8ba5d4476bc612a
zohaibmunawar <zohaibmunawar4777@gmail.com>
2024-07-17 17:50:13 +0500

    fix: db url

--- backend/config/database.js
+++ backend/config/database.js
@@ -11,7 +11,7 @@ module.exports = {
     // schema: config.schema,
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable: process.env.DATABASE_URL,
     dialect: "postgres",
     seederStorage: "sequelize",
     dialectOptions: {
